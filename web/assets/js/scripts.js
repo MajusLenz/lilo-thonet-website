fe6d3971d552fcc0725310ea32ad5a025ac9b40d
Majus Lenz <mariuslenzing@yahoo.de>
2019-01-15 17:06:31 +0100

    resize buttons programmiert.

--- web/assets/js/scripts.js
+++ web/assets/js/scripts.js
@@ -1,7 +1,7 @@
 $(function() {
 
-    // Mansory Grid:
-    var $grid = $('.grid').masonry({
+    // Masory Grid:
+    var $masoryGrid = $('.grid').masonry({
         // options
         itemSelector: '.grid-item',
         columnWidth: '.grid-sizer',
@@ -10,7 +10,8 @@ $(function() {
 
 
     // Lazy Loading:
-    $('.lazy').Lazy({
+    $lazyInstance = $('.lazy').Lazy({
+        chainable: false,
         scrollDirection: 'vertical',
         effect: 'fadeIn',
         effectTime: 350,
@@ -19,9 +20,9 @@ $(function() {
             console.log('error loading ' + element.data('src'));
         },
         afterLoad: function(element) {
-            //$(element).css("top", "0px");  // Animation
+            //$(element).css("top", "0px"); // Animation
 
-            $grid.masonry('layout');    // Mansory Grid wieder richtig ausrichten
+            $masoryGrid.masonry('layout');    // Masory Grid wieder richtig ausrichten
         }
     });
 
@@ -52,11 +53,67 @@ $(function() {
     });
 
 
-    // Image Zoom:
+    // Image Zoom (Desktop):
     $('.detail-zoom').each(function() {
         var $this = $(this);
         var bigImage = $this.data("zoom-image");
         $this.zoom({url: bigImage, magnify:0.5});
     });
+
+
+    // Resize-Buttons:
+    $(".resize-buttons button").on("click", function(e) {
+        var $this = $(this);
+        var active = "active-btn";
+        var inactive = "inactive-btn";
+
+        if($this.hasClass(active))
+            return;
+
+        $(".resize-buttons .active-btn").removeClass(active).addClass(inactive);
+        $this.removeClass(inactive).addClass(active);
+
+        var grid = "grid";
+        var gridGross = "grid-gross";
+        var gridMittel = "grid-mittel";
+        var gridKlein = "grid-klein";
+
+        var gridSizer = "grid-sizer";
+        var gridItem = "grid-item";
+        var gridItemGross = "grid-item-gross";
+        var gridItemMittel = "grid-item-mittel";
+        var gridItemKlein = "grid-item-klein";
+
+        var groesse = $this.data("size");
+
+        var grids = $("."+grid);
+        var gridItems = $("."+gridItem +", ."+ gridSizer);
+
+        grids.removeClass(gridKlein).removeClass(gridMittel).removeClass(gridGross);
+        gridItems.removeClass(gridItemKlein).removeClass(gridItemMittel).removeClass(gridItemGross);
+
+        switch(groesse) {
+            case "gross":
+                grids.addClass(gridGross);
+                gridItems.addClass(gridItemGross);
+                break;
+            case "mittel":
+                grids.addClass(gridMittel);
+                gridItems.addClass(gridItemMittel);
+                break;
+            case "klein":
+                grids.addClass(gridKlein);
+                gridItems.addClass(gridItemKlein);
+                break;
+        }
+
+        $masoryGrid.masonry('layout');    // Masory Grid wieder richtig ausrichten
+
+        setTimeout(function() {
+            $lazyInstance.update();     // Bilder nachladen, die nach Resize in Sichtfeld sind
+        }, 200);
+    });
+
+
 
 });
