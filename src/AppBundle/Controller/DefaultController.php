2bd5b4fd6f3a48b51bd7aafe9e414a6aae520917
Majus Lenz <mariuslenzing@yahoo.de>
2019-02-01 18:15:08 +0100

    sql suche weitergemacht.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -108,7 +108,7 @@ class DefaultController extends Controller
 
 
 
-        // Freitext-Suche
+        // Freitext-Suche:
         $sql =
             "SELECT a.* " .
             "FROM Archivierung a " .
@@ -116,16 +116,14 @@ class DefaultController extends Controller
             "LEFT JOIN Information i ON ai.information_id = i.id " .
             "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
             "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
-            "WHERE ( ( LOWER(i.wert) LIKE LOWER('%illustration%') AND i.name != 'Tags' ) OR LOWER(j.wert) LIKE LOWER('%illustration%') )"
+            "WHERE ( ( LOWER(i.wert) LIKE LOWER('%Illustration%') AND i.name != 'Tags' ) OR LOWER(j.wert) LIKE LOWER('%Illustration%') )"
         ;
-
         $rsm = new ResultSetMappingBuilder($em);
         $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
         $query = $em->createNativeQuery($sql, $rsm);
-        $archivierungenFreitext = $query->getResult();
+        $freitextSuche = $query->getResult();
 
-
-        // Filter-Suche
+        // Jahr-Filter:
         $sql =
             "SELECT a.* " .
             "FROM Archivierung a " .
@@ -133,25 +131,319 @@ class DefaultController extends Controller
             "LEFT JOIN Information i ON ai.information_id = i.id " .
             "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
             "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
-            "WHERE ( j.wert IN(1957) )"
+            "WHERE j.wert IN(1957)"
         ;
-
         $rsm = new ResultSetMappingBuilder($em);
         $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
         $query = $em->createNativeQuery($sql, $rsm);
-        $archivierungenFilter = $query->getResult();
+        $filterJahr = $query->getResult();
+
+        // Titel-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Titel')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterTitel = $query->getResult();
+
+        // Auftraggeber-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Auftraggeber')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterAuftraggeber = $query->getResult();
+
+        // Objektkategorie-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Objektkategorie')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $titelObjektkategorie = $query->getResult();
+
+        // Gestaltung-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Gestaltung')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterGestaltung = $query->getResult();
+
+        // Fotografie-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Fotografie')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterFotografie = $query->getResult();
+
+        // Druckerei-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Druckerei')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterDruckerei = $query->getResult();
+
+        // Autor-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Autor')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterAutor = $query->getResult();
+
+        // Material-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Material')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterMaterial = $query->getResult();
+
+        // Produktionsverfahren-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Produktionsverfahren')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterProduktionsverfahren = $query->getResult();
+
+        // Sprache-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Sprache')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterSprache = $query->getResult();
+
+        // Schrift-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Schrift')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterSchrift = $query->getResult();
+
+        // Farbe-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Farbe')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterFarbe = $query->getResult();
+
+        // Bilddarstellung-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Bilddarstellung')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterBilddarstellung = $query->getResult();
+
+        // Hinweis-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Hinweis')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterHinweis = $query->getResult();
+
+        // Produktkategorie-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Produktkategorie')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterProduktkategorie = $query->getResult();
+
+        // Möbel-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Moebel')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterMoebel = $query->getResult();
+
+        // Text-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Text')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterText = $query->getResult();
+
+        // Archiv-Filter:
+        $sql =
+            "SELECT a.* " .
+            "FROM Archivierung a " .
+            "LEFT JOIN Archivierung_Information ai ON a.id = ai.archivierung_id " .
+            "LEFT JOIN Information i ON ai.information_id = i.id " .
+            "LEFT JOIN Archivierung_Jahr aj ON a.id = aj.archivierung_id " .
+            "LEFT JOIN Jahr j ON aj.jahr_id = j.id " .
+            "WHERE (i.wert IN('Thonet 157') AND i.name = 'Archiv')"
+        ;
+        $rsm = new ResultSetMappingBuilder($em);
+        $rsm->addRootEntityFromClassMetadata('AppBundle:Archivierung', 'a');
+        $query = $em->createNativeQuery($sql, $rsm);
+        $filterArchiv = $query->getResult();
 
 
-        $archivierungen = array_uintersect($archivierungenFreitext, $archivierungenFilter, function($a1, $a2){
-            if($a1->getId() == $a2->getId())
-                return 0;
-            else
-                return 1;
-        });
+        // Schnittmenge aller Ergebnismengen:
+        $archivierungen = array_uintersect(
+            $freitextSuche,
+            $filterJahr,
+            $filterTitel,
+            $filterAuftraggeber,
+            $titelObjektkategorie,
+            $filterGestaltung,
+            $filterFotografie,
+            $filterDruckerei,
+            $filterAutor,
+            $filterMaterial,
+            $filterProduktionsverfahren,
+            $filterSprache,
+            $filterSchrift,
+            $filterFarbe,
+            $filterBilddarstellung,
+            $filterHinweis,
+            $filterProduktkategorie,
+            $filterMoebel,
+            $filterText,
+            $filterArchiv,
+
+            function($a1, $a2) {
+                if($a1->getId() == $a2->getId())
+                    return 0;
+                elseif($a1->getId() > $a2->getId())
+                    return 1;
+                else
+                    return -1;
+            }
+        );
 
 
-        $archivierung = $archivierungenFreitext;
-
+        //$archivierungen = $archivierungenFilter;
 
         foreach($archivierungen as $key => $archivierung) {
             $dateiHash = $archivierung->getDateiHash();
